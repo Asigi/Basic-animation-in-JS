3d79365aeb98ce59513d285768a69aa998b5fae3
arshdeep <arshsingh_sc@yahoo.com>
2016-04-21 13:31:07 -0700

    finished

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 
 
+/*
+  Computational World - Herobound gladators, Ansher Wars
+  JavaScript - interpreted language, not compiled into binary file
+  JavaScript provides interactive elements of web page
+  */
+
+//Purpose of Animation oject - track how long animation has been running, draw appropriate frame
 function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
     this.spriteSheet = spriteSheet;
     this.frameWidth = frameWidth;
@@ -34,25 +41,24 @@ Animation.prototype.drawFrame = function (tick, ctx, x, y) {
 
 Animation.prototype.currentFrame = function () {
     return Math.floor(this.elapsedTime / this.frameDuration);
-}
+};
 
 Animation.prototype.isDone = function () {
     return (this.elapsedTime >= this.totalTime);
-}
+};
 
 
 
 
-function Background(game) {
+
+function Background(game, backg) {
     this.game = game;
     this.ctx = game.ctx;
-    this.image = AM.getAsset("./img/bgd.jpg");
+    this.image = backg;
 }
-
 Background.prototype.draw = function (ctx) {
     ctx.drawImage(this.image, 0, 0);
 };
-
 Background.prototype.update = function () {};
 
 
@@ -61,82 +67,69 @@ Background.prototype.update = function () {};
 
 
 
-function Alien(game, spritesheet, direction) {
-    this.RightAnimation = new Animation(spritesheet, 0, 262, 262, 8, 0.10, 30, false, 1);
-    this.LeftAnimation = new Animation(spritesheet, 0, 262, 262, 8, 0.10, 30, false, 1);
+function Alien(game, sprite, direction) {
+	this.leftward = new Animation(sprite, 262, 262, 8, 0.03, 24, true, 1);
+  this.rightward = new Animation(sprite, 262, 262, 8, 0.03, 24, true, 2);
+  this.game = game;
+	this.direction = direction;
+  this.ctx = game.ctx;
 
-    this.game = game;
-  	this.direction = direction;
-    this.ctx = game.ctx;
-
-    if (direction == "left") {
-        this.x = 900;
-  	    this.speed = 100;
-  	    this.y  = 200;
-    } else  { //must be right
-        this.x = 100;
-        this.speed = 100;
-        this.y  = 400;
-    }
+  if (direction == "left") {
+      this.x = 1000;
+      this.speed = 200;
+      this.y  = 140;
+  } else if (direction == "right") {
+      this.x = -200;
+      this.speed = 100;
+      this.y = 210;
+  }
 }
 
+Alien.prototype.update = function() {
+
+	if(this.direction == "right") {
+		this.x += this.game.clockTick * this.speed;
+	} else { //left
+		this.x -= this.game.clockTick * this.speed;
+	}
+
+
+	if(this.x > 1100 || this.x < -300) {
+    if (this.direction == "left") {
+        this.x = 1100;
+    } else{
+        this.x = -300;
+    }
+	}
+
+};
 
 Alien.prototype.draw = function (ctx) {
-	if(this.direction == "right") {
-		  this.RightAnimation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-	} else { //must be going left
-      this.LeftAnimation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+	if(this.direction == "left") {
+		this.leftward.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+	} else {
+    this.rightward.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
   }
 };
 
 
-Alien.prototype.update = function() {
-	if(this.direction == "right") {
-		  this.x += this.game.clockTick * this.speed;
-	} else { //must be going left
-		  this.x -= this.game.clockTick * this.speed;
-	}
-
-	if(this.x > 1000 || this.x < 0) {
-      if (this.direction = "left") {
-        this.x = 900;
-      } else{
-		      this.x = 100;
-      }
-	}
-	if(this.y > 600 || this.y < 0) {
-      if (this.direction = "left") {
-          this.y = 200;
-      } else{
-          this.y = 400;
-      }
-	}
-};
 
 
 
-
-
-//main
 var AM = new AssetManager();
-
 AM.queueDownload("./img/alienSprite.png");
 AM.queueDownload("./img/bgd.jpg");
 
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
 
-    //2d
     var ctx = canvas.getContext("2d");
-
     var gameEngine = new GameEngine();
     gameEngine.init(ctx);
     gameEngine.start();
 
-    gameEngine.addEntity(new Background(gameEngine));
+    gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/bgd.jpg")));
+    gameEngine.addEntity(new Alien(gameEngine, AM.getAsset("./img/alienSprite.png"), "left"));
+    gameEngine.addEntity(new Alien(gameEngine, AM.getAsset("./img/alienSprite.png"), "right"));
 
-    //gameEngine.addEntity(new Alien(gameEngine, AM.getAsset("./img/alienSprite.png"), "left"));
-    //gameEngine.addEntity(new Alien(gameEngine, AM.getAsset("./img/alienSprite.png"), "right"));
-
-    console.log("All Done!");
 });
